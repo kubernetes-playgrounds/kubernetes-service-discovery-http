9647019f32ede5544f74bff3cd1ca5cb692305e6
guanw <guanw0826@gmail.com>
2020-01-30 12:04:55 -0500

    update client to not panic

--- client/main.go
+++ client/main.go
@@ -11,16 +11,18 @@ func main() {
 	for {
 		resp, err := http.Get("http://server-svc:8081/Jude")
 		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
-		fmt.Println("Response status:", resp.Status)
-		scanner := bufio.NewScanner(resp.Body)
-		for i := 0; scanner.Scan() && i < 5; i++ {
-			fmt.Println(scanner.Text())
-		}
-		if err := scanner.Err(); err != nil {
-			panic(err)
+			fmt.Println(err.Error())
+		} else {
+			defer resp.Body.Close()
+			fmt.Println("Response status:", resp.Status)
+			scanner := bufio.NewScanner(resp.Body)
+			for i := 0; scanner.Scan() && i < 5; i++ {
+				fmt.Println(scanner.Text())
+			}
+			if err := scanner.Err(); err != nil {
+				fmt.Println(err.Error())
+			}
+
 		}
 		time.Sleep(5 * time.Second)
 	}
